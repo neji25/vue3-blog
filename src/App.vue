7f3e6570a6159438ae3ba44dd727c5c32b930c94
Vlad <myborvlad@gmail.com>
2023-06-20 16:27:30 +0300

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <template>
   <div class="app">
-    <form @submit.prevent>
-      <h4>Создание поста</h4>
-      <input
-          v-bind:value="title"
-          @input="title = $event.target.value"
-          class="input" type="text"
-          placeholder="Название"/>
-      <input
-          v-bind:value="body"
-          @input="body = $event.target.value"
-          class="input" type="text"
-          placeholder="Описание"/>
-      <button
-          class="btn"
-          @click="createPost"
-      >
-        Создать</button>
-    </form>
-    <div class="post" v-bind:key="post.id" v-for="post in posts">
-      <div><strong>Название:</strong> {{post.title}}</div>
-      <div><strong>Описание:</strong> {{post.body}}</div>
-    </div>
+    <post-form
+        @create="createPost"
+    />
+    <post-list
+        :posts="posts"
+
+    />
   </div>
 </template>
 
 <script>
+import PostList from "@/components/PostList.vue";
+import PostForm from "@/components/PostForm.vue";
 export default {
+  components: {
+    PostList,
+    PostForm
+  },
   data() {
     return {
       posts: [
@@ -39,15 +30,8 @@ export default {
     }
   },
   methods: {
-    createPost() {
-      const newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body
-      }
-      this.posts.push(newPost)
-      this.title = ''
-      this.body = ''
+    createPost(post) {
+      this.posts.push(post)
     },
   }
 }
@@ -62,31 +46,5 @@ export default {
 
   .app {
     padding: 20px;
-  }
-
-  form {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .btn {
-    margin-top: 15px;
-    align-self: flex-end;
-    padding: 10px 15px;
-    background: none;
-    color: teal;
-    border: 1px solid teal;
-  }
-
-  .post {
-    padding: 15px;
-    border: 2px solid teal;
-    margin-top: 15px;
-  }
-  .input {
-    width: 100%;
-    border: 1px solid teal;
-    padding: 10px 15px;
-    margin-top: 15px;
   }
 </style>
