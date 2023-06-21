948da72b27dceb7426dded3c2ce8fb2d5f4118bb
Vlad <myborvlad@gmail.com>
2023-06-21 11:50:49 +0300

    Added modal for new post

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="app">
-    <post-form
-        @create="createPost"
-    />
+    <h1>Страница с постами</h1>
+    <my-button
+        class="btn-primary"
+        style="margin: 15px 0;"
+        @click="showDialog"
+    >Создать пост</my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+          @create="createPost"
+      />
+    </my-dialog>
     <post-list
         :posts="posts"
         @remove="removePost"
@@ -13,8 +21,12 @@
 <script>
 import PostList from "@/components/PostList.vue";
 import PostForm from "@/components/PostForm.vue";
+import MyDialog from "@/components/UI/MyDialog.vue";
+import MyButton from "@/components/UI/MyButton.vue";
 export default {
   components: {
+    MyButton,
+    MyDialog,
     PostList,
     PostForm
   },
@@ -27,14 +39,19 @@ export default {
       ],
       title: '',
       body: '',
+      dialogVisible: false
     }
   },
   methods: {
     createPost(post) {
       this.posts.push(post)
+      this.dialogVisible = false
     },
     removePost(post) {
       this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true
     }
   }
 }
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,16 @@
 <template>
-  <h3>Список пользователей</h3>
-  <post-item
-      v-for="post in posts"
-      :key="post.id"
-      :post="post"
-      @remove="$emit('remove', post)"
-  />
+  <div v-if="posts.length > 0">
+    <h3>Список постов</h3>
+    <post-item
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+        @remove="$emit('remove', post)"
+    />
+  </div>
+  <h2 v-else style="color: red">
+    Список постов пуст
+  </h2>
 </template>
 
 <script>
